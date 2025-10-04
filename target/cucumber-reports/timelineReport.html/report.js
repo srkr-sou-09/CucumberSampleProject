7326e857aa74018fafb2a0e573bb80ae242a873a
srkr.sou.09 <srkr.sou.09@outlook.com>
2025-10-05 01:24:48 +0530

    updating maven.yml

---
+++ target/cucumber-reports/timelineReport.html/report.js
@@ -0,0 +1,155 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "7d7c4def-7225-4e20-a61d-cbfa3d27cd07",
+    "feature": "As a user of application I want to validate login functionality",
+    "scenario": "Validate login functionality with invalid credentials",
+    "start": 1759594961263,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1759594975596,
+    "className": "failed"
+  },
+  {
+    "id": "6d1c423a-1906-41bc-a30c-974079539fab",
+    "feature": "As a user of application I want to validate login functionality",
+    "scenario": "Validate login functionality with valid credentials",
+    "start": 1759594956902,
+    "group": 21,
+    "content": "",
+    "tags": "@validcredentials,",
+    "end": 1759594971272,
+    "className": "passed"
+  },
+  {
+    "id": "d1578dda-c758-4f40-a3ce-9b526dc86476",
+    "feature": "Validate dashboards",
+    "scenario": "Validate login functionality with valid credentials 3",
+    "start": 1759594956904,
+    "group": 19,
+    "content": "",
+    "tags": "@smoke,@validcredentials,",
+    "end": 1759594961413,
+    "className": "passed"
+  },
+  {
+    "id": "1713ad20-20e4-45b6-876e-1126848e0861",
+    "feature": "Validate dashboards",
+    "scenario": "Validate login functionality with valid credentials",
+    "start": 1759594956904,
+    "group": 17,
+    "content": "",
+    "tags": "@smoke,@validcredentials,",
+    "end": 1759594966404,
+    "className": "passed"
+  },
+  {
+    "id": "1b7f6804-fa1a-40ae-b2aa-9c8ff7295c7d",
+    "feature": "As a user of application I want to validate login functionality",
+    "scenario": "Validate login functionality with invalid credentials",
+    "start": 1759594956897,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1759594961226,
+    "className": "failed"
+  },
+  {
+    "id": "bf52252b-4ebc-4e3d-8423-2440aca21a08",
+    "feature": "As a user of application I want to validate login functionality",
+    "scenario": "Validate login functionality with valid credentials",
+    "start": 1759594956899,
+    "group": 23,
+    "content": "",
+    "tags": "@validcredentials,",
+    "end": 1759594971414,
+    "className": "passed"
+  },
+  {
+    "id": "ca7cc659-f8aa-473d-90fb-10594f2cc4e3",
+    "feature": "As a user of application I want to validate login functionality",
+    "scenario": "Validate login functionality with invalid credentials",
+    "start": 1759594977867,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1759594980214,
+    "className": "failed"
+  },
+  {
+    "id": "691aa029-e299-4daa-984f-4dd77308d675",
+    "feature": "As a user of application I want to validate login functionality",
+    "scenario": "Validate login functionality with invalid credentials",
+    "start": 1759594980220,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1759594982982,
+    "className": "failed"
+  },
+  {
+    "id": "45bd4e47-854f-4127-a0c5-e86a0ca99fca",
+    "feature": "As a user of application I want to validate login functionality",
+    "scenario": "Validate login functionality with valid credentials",
+    "start": 1759594956906,
+    "group": 20,
+    "content": "",
+    "tags": "@smoke,@maintestcase,@validcredentials,",
+    "end": 1759594971598,
+    "className": "passed"
+  },
+  {
+    "id": "98633ccd-47e8-4536-848e-0b77798adb11",
+    "feature": "As a user of application I want to validate login functionality",
+    "scenario": "Validate login functionality with invalid credentials",
+    "start": 1759594975602,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1759594977859,
+    "className": "failed"
+  },
+  {
+    "id": "2c2adbe3-6cb0-4467-a222-db77677123fd",
+    "feature": "Validate dashboards",
+    "scenario": "Validate login functionality with valid credentials 2",
+    "start": 1759594956907,
+    "group": 18,
+    "content": "",
+    "tags": "@smoke,@validcredentials,",
+    "end": 1759594970591,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 20,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 21,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
+  },
+  {
+    "id": 22,
+    "content": "Thread[TestNG-PoolService-5,5,main]"
+  },
+  {
+    "id": 23,
+    "content": "Thread[TestNG-PoolService-6,5,main]"
+  }
+]);
+});
