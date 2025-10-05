5d8fd1836a791271ed11c60d015de5fe4c3d4f97
srkr.sou.09 <srkr.sou.09@outlook.com>
2025-10-06 03:31:19 +0530

    Cross Browser testing implemented

--- src/test/java/browserManager/FireFoxManager.java
+++ src/test/java/browserManager/FireFoxManager.java
@@ -8,7 +8,16 @@ public class FireFoxManager implements BrowserManager{
 
     @Override
     public WebDriver getDriver() {
-        WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver();
+        try{
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+
+        }catch(Exception ex){
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")
+                    +"/src/test/resources/drivers/geckodriver");
+            return new FirefoxDriver();
+        }
+
+
     }
 }
