9ef5ccfefaf9f371aa21d253af56832b83476aad
srkr.sou.09 <srkr.sou.09@outlook.com>
2025-10-06 02:44:34 +0530

    Cross Browser testing implemented

---
+++ src/test/java/browserManager/EdgeManager.java
@@ -0,0 +1,18 @@
+package browserManager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeManager implements BrowserManager {
+    @Override
+    public WebDriver getDriver() {
+        try{
+            WebDriverManager.edgedriver().setup();
+            return new EdgeDriver();
+        }catch (Exception ex){
+            System.setProperty("webdriver.edge.driver","/src/test/resources/drivers/msedgedriver.exe");
+            return new EdgeDriver();
+        }
+    }
+}
